6818f09f67659a1b953e1cbd6b822dec1eaa1a1e
SuryanshShahi <suryansh06shahi@gmail.com>
2022-08-24 12:27:43 +0530

    Synergy account

--- mail.php
+++ mail.php
@@ -4,15 +4,15 @@ $username = $_POST['name'];
 $number = $_POST['tel'];
 $email = $_POST['email'];
 $msg = $_POST['message'];
-$to = "azib.quantumit@gmail.com";
-$subject = "Sweethill Leads";
+$to = "Synergyshred@gmail.com , shefali.quantum@gmail.com ";
+$subject = "Synergy Shred";
 
 
 
 $message = "
 <html>
 <head>
-<title>Sweethill Leads</title>
+<title>Synergy Shred</title>
 </head>
 <body>
 <h3>Name: $username</h3>
@@ -26,9 +26,9 @@ $message = "
 // Always set content-type when sending HTML email
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers .= "Reply-To: The Sender <contact@sweet-hill.com>\r\n";
- $headers .= "Return-Path: The Sender <citiconcretecompany@gmail.com>\r\n";
-$headers .= "From: Sweethill <contact@sweet-hill.com>\r\n";
+$headers .= "Reply-To: The Sender <Synergyshred@gmail.com>\r\n";
+ $headers .= "Return-Path: The Sender <Synergyshred@gmail.com>\r\n";
+$headers .= "From: SynergyShred <Synergyshred@gmail.com>\r\n";
 
 if (mail($to,$subject,$message,$headers))
 {
